caaee3d06c05263e45173aeacbc0e295bb3a1206
leonardoschulze <leonardo_schulze@estudante.sesisenai.org.br>
2025-11-10 08:32:51 -0300

    UPDATE - definido as variaveis pros topicos e feito o getmessagens

--- getmessagens.php
+++ getmessagens.php
@@ -4,11 +4,18 @@ include_once(".env.php");
 
 $server = $_DB_HOST;
 $port = $_DB_PORT;
-$topic = $_DB_TOPIC;
+$topic = $_DB_ILUM;
+$topic1 = $_DB_PRESENCA1;
+$topic2 = $_DB_PRESENCA2;
+$topic3 = $_DB_TEMP;
+$topic4 = $_DB_UMID;
+$topic5 = $_DB_PRESENCAS1;
+$topic6 = $_DB_TREM;
+$topic7 = $_DB_STATUS;
 $client_id = "phpmqtt-" . rand();
 
-$username = "";
-$password = "";
+$username = $_DB_USER;
+$password = $_DB_PASS;
 
 header('Content-Type: application/json');
 
@@ -23,6 +30,34 @@ if (!$mqtt->connect(true, NULL, $username, $password)) {
 // Subscribing e coletando mensagens por 1-2 segundos
 $mqtt->subscribe([$topic => ["qos" => 0, "function" => function ($topic, $msg) use (&$messages) {
     $messages[] = ["topic" => $topic, "msg" => $msg, "time" => date("H:i:s")];
+}]], 0);
+
+$mqtt->subscribe([$topic1 => ["qos" => 0, "function" => function ($topic1, $msg) use (&$messages) {
+    $messages[] = ["topic" => $topic1, "msg" => $msg, "time" => date("H:i:s")];
+}]], 0);
+
+$mqtt->subscribe([$topic2 => ["qos" => 0, "function" => function ($topic2, $msg) use (&$messages) {
+    $messages[] = ["topic" => $topic2, "msg" => $msg, "time" => date("H:i:s")];
+}]], 0);
+
+$mqtt->subscribe([$topic3 => ["qos" => 0, "function" => function ($topic3, $msg) use (&$messages) {
+    $messages[] = ["topic" => $topic3, "msg" => $msg, "time" => date("H:i:s")];
+}]], 0);
+
+$mqtt->subscribe([$topic4 => ["qos" => 0, "function" => function ($topic4, $msg) use (&$messages) {
+    $messages[] = ["topic" => $topic4, "msg" => $msg, "time" => date("H:i:s")];
+}]], 0);
+
+$mqtt->subscribe([$topic5 => ["qos" => 0, "function" => function ($topic5, $msg) use (&$messages) {
+    $messages[] = ["topic" => $topic5, "msg" => $msg, "time" => date("H:i:s")];
+}]], 0);
+
+$mqtt->subscribe([$topic6 => ["qos" => 0, "function" => function ($topic6, $msg) use (&$messages) {
+    $messages[] = ["topic" => $topic6, "msg" => $msg, "time" => date("H:i:s")];
+}]], 0);
+
+$mqtt->subscribe([$topic7 => ["qos" => 0, "function" => function ($topic7, $msg) use (&$messages) {
+    $messages[] = ["topic" => $topic7, "msg" => $msg, "time" => date("H:i:s")];
 }]], 0);
 
 $start = time();
